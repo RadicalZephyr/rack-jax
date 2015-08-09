2ae3492412171d58f5c5a0ecca10a7c8a39d72c4
Geoff Shannon <geoffpshannon@gmail.com>
2015-08-09 16:51:06 -0500

    Add specs for including the request method

--- lib/rack_jax/app_wrapper.rb
+++ lib/rack_jax/app_wrapper.rb
@@ -5,10 +5,16 @@ module RackJax
   class AppWrapper
 
     def initialize(app)
+      @app = app
     end
 
     def handle(request)
+      env = {"REQUEST_METHOD" => request.method.to_s}
+      app.call(env)
       {}
     end
+
+    private
+    attr_reader :app
   end
 end
