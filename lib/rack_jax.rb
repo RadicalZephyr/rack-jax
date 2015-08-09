73f6c09a2a860cb2aa6edd3de33302ae45a4781b
Zefira Shannon <earthlingzephyr@gmail.com>
2015-08-09 17:50:33 -0500

    Stub out a new InputWrapper class

--- lib/rack_jax.rb
+++ lib/rack_jax.rb
@@ -3,3 +3,4 @@ require 'jax-server-0.1.0.jar'
 
 require 'rack_jax/version'
 require 'rack_jax/app_wrapper'
+require 'rack_jax/input_wrapper'
---
+++ spec/rack_jax/input_wrapper_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe RackJax::InputWrapper do
+
+end
