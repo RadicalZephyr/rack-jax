f5d4967513b0a4506ed57846e0f72ae9c08e5a30
Zefira Shannon <earthlingzephyr@gmail.com>
2015-08-09 18:41:01 -0500

    Get line reading working with input wrapper

--- lib/rack_jax/input_wrapper.rb
+++ lib/rack_jax/input_wrapper.rb
@@ -1,5 +1,33 @@
 module RackJax
 
   class InputWrapper
+
+    def initialize(bytes)
+      @byte_reader = java_io::ByteArrayInputStream.new(bytes.array)
+    end
+
+    def get
+      line_reader.read_line
+    end
+
+    private
+    attr_reader :byte_reader
+
+    def line_reader
+      @line_reader ||=
+        java_io::BufferedReader.new(
+        java_io::InputStreamReader.new(byte_reader,
+                                       standard_charsets::US_ASCII))
+
+      @line_reader
+    end
+
+    def java_io
+      Java::JavaIo
+    end
+
+    def standard_charsets
+      Java::JavaNioCharset::StandardCharsets
+    end
   end
 end
--- spec/rack_jax/input_wrapper_spec.rb
+++ spec/rack_jax/input_wrapper_spec.rb
@@ -2,4 +2,31 @@ require 'spec_helper'
 
 describe RackJax::InputWrapper do
 
+  def byte_buffer(text)
+    Java::JavaNio::ByteBuffer.wrap(text.join("\n").to_java_bytes)
+  end
+
+  let(:input) {described_class.new(byte_buffer(lines))}
+
+  let(:first_line) {'stuff and text'}
+  let(:lines) {[first_line]}
+
+  it 'can get by lines' do
+    expect(input.get).to eq(first_line)
+  end
+
+  it 'returns nil once all input is read' do
+    input.get
+    expect(input.get).to be_nil
+  end
+
+  context 'with multiple lines' do
+    let(:second_line) {'more stuff, but no things'}
+    let(:lines) {[first_line, second_line]}
+
+    it 'can get the second line' do
+      expect(input.get).to eq(first_line)
+      expect(input.get).to eq(second_line)
+    end
+  end
 end
