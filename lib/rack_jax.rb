f5cb0695067f31b2f0ffb213fa3b0744496cc2dc
Geoff Shannon <geoffpshannon@gmail.com>
2015-08-10 13:50:22 -0500

    Change require dependencies around
    the handler/rack_jax file gets auto-required when that server is
    requested, so it should require the rest of the rack_jax libs, not the
    other way around.

--- lib/rack_jax.rb
+++ lib/rack_jax.rb
@@ -4,8 +4,3 @@ require 'jax-server-0.1.0.jar'
 require 'rack_jax/version'
 require 'rack_jax/app_wrapper'
 require 'rack_jax/input_wrapper'
-
-require 'rack/handler/rack_jax'
-require 'rack/handler'
-
-Rack::Handler.register('rack_jax', 'Rack::Handler::RackJax')
